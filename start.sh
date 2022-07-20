4964ee26a12535bf4b7eb768ab71d75031041010
xyzonecodes <xyzone@vip.qq.com>
2022-07-20 15:05:41 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,13 +5,34 @@ source /app/config.sh
 
 # echo "安装 htop等"
 # apt-get install htop -y
+
+
+# cp -r /usr/share/nginx/html/x-ui/db/x-ui.db /etc/x-ui/
+echo "nezha 面板"
+cd ~
+wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh
+mv install.sh nezha.sh
+echo "nezha 面板 ls "
+ls
+chmod +x nezha.sh
+# ./nezha.sh
+./nezha.sh install_agent $NEZHA_IP 12728 $NEZHAAGENT
+# curl -L https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -o nezha.sh && chmod +x nezha.sh && ./nezha.sh install_agent kra2.xyzone.eu.org 12728 de2e65cb080ec0a2a4
+# 0
+echo "nohup /opt/nezha/agent/nezha-agent -s $NEZHA_IP:12728 -p $NEZHAAGENT &"
+echo "nohup /opt/nezha/agent/nezha-agent -s $NEZHA_IP:12728 -p $NEZHAAGENT &"
+nohup /opt/nezha/agent/nezha-agent -s $NEZHA_IP:12728 -p $NEZHAAGENT &
+
+
 echo "安装 x-ui"
+cd ~
 wget https://github.com/vaxilu/x-ui/releases/download/0.3.2/x-ui-linux-amd64.tar.gz
 tar -zxvf x-ui-linux-amd64.tar.gz
 chmod +x x-ui/x-ui x-ui/bin/xray-linux-* x-ui/x-ui.sh
 cp x-ui/x-ui.sh /usr/bin/x-ui
 cp -f x-ui/x-ui.service /etc/systemd/system/
 mv x-ui/ /usr/local/
+
 # systemctl daemon-reload
 # systemctl enable x-ui
 # systemctl restart x-ui
@@ -20,6 +41,7 @@ mv x-ui/ /usr/local/
 # nginx -s reload
 # # mkdir /etc/x-ui/
 # # cp -r /usr/share/nginx/html/x-ui/db/x-ui.db /etc/x-ui/
+
 cd /usr/local/x-ui/bin
 echo "启动xray"
 nohup ./xray-linux-amd64 &
@@ -28,6 +50,8 @@ echo "启动面板"
 chmod +x ./x-ui
 ls
 nohup ./x-ui &
+
+
 # # cp -r /usr/share/nginx/html/x-ui/db/x-ui.db /etc/x-ui/
 # echo "nezha 面板"
 # wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh
